786c21998106bc33b6038676e69e5aa4e35fb4aa
Grey <ingrid.freguis@gmail.com>
2025-07-13 18:18:37 +0200

    create path show

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -3,14 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Movie;
+use App\Entity\Review;
+use App\Form\ReviewType;
 use App\Repository\MovieRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class MovieController extends AbstractController
 {
-    #[Route('/movie', name: 'app_movie')]
+    #[Route('/films', name: 'app_movie')]
     public function index(MovieRepository $movieRepository): Response
     {
 
@@ -20,6 +22,20 @@ final class MovieController extends AbstractController
 
         return $this->render('movie/index.html.twig', [
             'movies' => $movies,
+        ]);
+    }
+
+    #[Route('/films/{id}', name: 'app_movie_show', requirements: ['id' => '\d+'])]
+    public function show(Movie $movie): Response
+    {
+        /* dump($movie); */
+
+        $review = new Review();
+        $form= $this->createForm(ReviewType::class, $review );
+
+        return $this->render('movie/show.html.twig', [
+            'movie' => $movie,
+            'form' => $form,
         ]);
     }
 }
