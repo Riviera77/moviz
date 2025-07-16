1130692000fa1659afd9833fe92dd7960cbb6b7c
Grey <ingrid.freguis@gmail.com>
2025-07-16 18:51:43 +0200

    modify necessary fields

--- src/Form/ReviewType.php
+++ src/Form/ReviewType.php
@@ -7,25 +7,32 @@ use App\Entity\Review;
 use App\Entity\UserAccount;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Choice;
 
 class ReviewType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('rate')
+            ->add('rate', ChoiceType::class, [
+                'label' => 'Votre note',
+                'choices' => [
+                    '5' => 5,
+                    '4' => 4,
+                    '3' => 3,
+                    '2' => 2,
+                    '1' => 1,                  
+                ],
+                'attr' => [
+                    'class' => 'rate'
+                ],
+                'expanded' => true, // Display as radio buttons
+                'multiple' => false, // Single choice
+            ])
             ->add('review')
-            ->add('approved')
-            ->add('userAccount', EntityType::class, [
-                'class' => UserAccount::class,
-                'choice_label' => 'id',
-            ])
-            ->add('movie', EntityType::class, [
-                'class' => Movie::class,
-                'choice_label' => 'id',
-            ])
         ;
     }
 
